710f1d5e3d7e1a33219a4e3f01e638134fce7c77
paeltech <pmandele9@gmail.com>
2018-02-13 19:04:32 +0300

    Added a mechanism to store sensor data to a file in json format, changed print statement in console and ranges in loops

--- Summative_assesment.py
+++ Summative_assesment.py
@@ -1,5 +1,14 @@
 #Import random library to gerenate random sensor data
 import random as rand
+
+#Import Json Library to be used to write sensor data in a file
+import json
+
+#Import date time and time library to add date time to sensor data when saving in file
+import datetime
+import time
+import calendar
+
 #create a dictionary to handle sensor data
 sensor_cluster = dict()
 
@@ -8,7 +17,20 @@ sensor_cluster = dict()
 for i in range(32):
     sensor_data = rand.random()
     #Generate 16 random sensor data for every sensor cluster
-    sensor_cluster[i] = { n: [rand.random()] for n in range(16+1)}
+    sensor_cluster[i] = { n: [rand.random()] for n in range(16)}
 
-for k in sensor_cluster:
-    print sensor_cluster[k]
+
+#Store sensor data in a Json file
+with open("e://Dev//summative_assesment//venv//sensor_data.txt", "a") as f:
+    now = datetime.datetime.today()
+    f.write("Sensor data as of {}".format(now))
+    for row in sensor_cluster:
+        f.write("\n\nSensor data for cluster {}\n".format(row))
+        json.dump(sensor_cluster[row], f)
+    f.write("\n\n\n\n")
+
+#print sensor data in console
+print ("sensor data as of {}".format(now))
+for row in sensor_cluster:
+    print("sensor data for cluster {}".format(row))
+    print sensor_cluster[row]
